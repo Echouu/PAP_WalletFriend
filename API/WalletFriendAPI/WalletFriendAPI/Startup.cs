c1ccfdd814cc2e97b6903e99722f003fe41008e5
Artur Gameiro <artur.gameiro@hotmail.com>
2022-02-18 11:01:25 +0000

    commit

--- API/WalletFriendAPI/WalletFriendAPI/Startup.cs
+++ API/WalletFriendAPI/WalletFriendAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,26 @@ namespace WalletFriendAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(options =>
+            {
+                // definir e identificar a politica de CORS
+                // neste exemplo vamos aceitar todos os tipos de pedidos
+                options.AddPolicy("AllowBrowserApps",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin();
+                        policy.AllowAnyMethod();
+                        policy.AllowAnyHeader();
+                    });
+            });
+
+
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WalletFriend", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +54,11 @@ namespace WalletFriendAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WalletFriend v1"));
             }
+
+            app.UseCors("AllowBrowserApps");
 
             app.UseHttpsRedirection();
 
