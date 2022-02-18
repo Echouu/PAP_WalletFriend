ad400339296cf86718642734e25fba9c004d6e96
Artur Gameiro <artur.gameiro@hotmail.com>
2022-02-18 09:40:49 +0000

    commit

---
+++ API/WalletFriendAPI/WalletFriendAPI/Models/UserToAuth.cs
@@ -0,0 +1,10 @@
+﻿namespace WalletFriendAPI.Models
+{
+    public class UserToAuth
+    {
+        public string Username { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
